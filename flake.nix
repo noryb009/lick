23ff55cf6fe171509ad3b926b64897650fdf2776
Lukas Lorimer <lukas.lorimer@gmail.com>
2022-12-30 02:16:30 -0800

    Most of Windows build

--- flake.nix
+++ flake.nix
@@ -22,36 +22,53 @@
 
   outputs = { self, nix, nixpkgs, fltk_tarball, libarchive_tarball, catch_header }: (
     let
-      systems = [ "x86_64-linux" ];
-      forEachSystem = f: (nixpkgs.lib.genAttrs systems (system: (f system)));
+      systems = [ "x86_64-linux" "mingw32" ];
+      forEachItem = items: f: (nixpkgs.lib.genAttrs items (item: (f item)));
+      forEachSystem = forEachItem systems;
+      project = nix_pkgs: {
+        name = "lick";
+        src = self;
+        buildInputs = [
+          nix_pkgs.pkgs.cmake
+          # Only needed in Linux:
+          #nix_pkgs.pkgs.xorg.libX11
+          # TODO: This breaks cross-compilation
+          #nix_pkgs.pkgs.fltk
+        ];
+        configurePhase = ''
+          cmake .
+          mkdir -p libarchive-3.3.1-prefix/src/
+          cp ${libarchive_tarball} libarchive-3.3.1-prefix/src/libarchive-3.3.1.tar.gz
+          mkdir -p fltk-1.3.5-prefix/src/
+          cp ${fltk_tarball} fltk-1.3.5-prefix/src/fltk-1.3.5-source.tar.bz2
+          mkdir -p test/catch-2.13.9/src/
+          cp ${catch_header} test/catch-2.13.9/src/catch.hpp
+        '';
+        buildPhase = ''
+          make
+        '';
+        installPhase = ''
+          make package
+          mkdir -p $out
+          install -t $out LICK-*.zip;
+          # Linux does not create `.exe` files.
+          to_install=(LICK-*.exe)
+          for f in "''${to_install[@]}"; do install -t $out "$f"; done
+        '';
+      };
     in {
-      defaultPackage = (
+      packages = (
         forEachSystem (system: (
-          let nix_pkgs = (import nixpkgs { system = "${system}"; }); in (
-            nix_pkgs.stdenv.mkDerivation {
-              name = "lick";
-              src = self;
-              buildInputs = [ nix_pkgs.pkgs.cmake nix_pkgs.pkgs.xorg.libX11 nix_pkgs.pkgs.fltk ];
-              configurePhase = ''
-                cmake .
-                mkdir -p libarchive-3.3.1-prefix/src/
-                cp ${libarchive_tarball} libarchive-3.3.1-prefix/src/libarchive-3.3.1.tar.gz
-                mkdir -p fltk-1.3.5-prefix/src/
-                cp ${fltk_tarball} fltk-1.3.5-prefix/src/fltk-1.3.5-source.tar.bz2
-                mkdir -p test/catch-2.13.9/src/
-                cp ${catch_header} test/catch-2.13.9/src/catch.hpp
-              '';
-              buildPhase = ''
-                make
-              '';
-              installPhase = ''
-                make package
-                mkdir -p $out
-                install -t $out LICK-*.zip;
-                # Linux does not create `.exe` files.
-                to_install=(LICK-*.exe)
-                for f in "''${to_install[@]}"; do install -t $out "$f"; done
-              '';
+          let
+            nix_pkgs = (import nixpkgs { system = "${system}"; });
+            otherSystems = (nixpkgs.lib.filter (x: x != system) systems);
+            forEachOtherSystem = forEachItem otherSystems;
+          in (
+            (nix_pkgs.stdenv.mkDerivation ((project nix_pkgs))) // {
+              pkgsCross = forEachOtherSystem (target: (
+                let crossPkgs = nix_pkgs.pkgsCross.mingw32; # TODO: Use `target`.
+                in crossPkgs.stdenv.mkDerivation (project crossPkgs)
+              ));
             }
           )
         ))
